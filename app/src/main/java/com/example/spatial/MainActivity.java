9b8a93c041bc6bcb04189c1797200b15af263cdd
sabrina <sabrinaawuni@yahoo.com>
2020-03-26 15:23:01 +0000

    second commit

--- app/src/main/java/com/example/spatial/MainActivity.java
+++ app/src/main/java/com/example/spatial/MainActivity.java
@@ -1,24 +1,58 @@
 package com.example.spatial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView webView = null;
+    public class GeoWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    public class GeoWebChromeClient extends WebChromeClient{
+        @Override
+        public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+            callback.invoke(origin, true,  false);
+        }
+    }
+
+    WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.webView = (WebView) findViewById(R.id.webView);
+        mWebView = (WebView) findViewById(R.id.webView);
 
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.setWebViewClient(new GeoWebViewClient());
 
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setGeolocationEnabled(true);
+        mWebView.setWebChromeClient(new GeoWebChromeClient());
 
-        webView.loadUrl("http://2633cb52.ngrok.io/geoserver/www/map.html");
+        mWebView.loadUrl("https://7d8ead23.ngrok.io/geoserver/www/map.html");
+
+    }
+
+    public void onBackPressed(){
+        if(mWebView.canGoBack()){
+            mWebView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
