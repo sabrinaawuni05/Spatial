51029eb50524d520b0971ae3815d98e48094da96
sabrina <sabrinaawuni@yahoo.com>
2020-03-29 22:02:20 +0000

    third commit

--- app/src/main/java/com/example/spatial/MainActivity.java
+++ app/src/main/java/com/example/spatial/MainActivity.java
@@ -1,57 +1,131 @@
 package com.example.spatial;
-
+import android.Manifest;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.content.pm.PackageManager;
 import androidx.appcompat.app.AppCompatActivity;
-import android.webkit.GeolocationPermissions;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
+
+
+import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
-import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.GeolocationPermissions;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * WebViewClient subclass loads all hyperlinks in the existing WebView
+     */
     public class GeoWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-
+            // When user clicks a hyperlink, load in the existing WebView
             view.loadUrl(url);
             return true;
         }
     }
 
-    public class GeoWebChromeClient extends WebChromeClient{
+    /**
+     * WebChromeClient subclass handles UI-related calls
+     * Note: think chrome as in decoration, not the Chrome browser
+     */
+    public class GeoWebChromeClient extends WebChromeClient {
         @Override
-        public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
-            callback.invoke(origin, true,  false);
+        public void onGeolocationPermissionsShowPrompt(String origin,
+                                                       GeolocationPermissions.Callback callback) {
+            // Always grant permission since the app itself requires location
+            // permission and the user has therefore already granted it
+            callback.invoke(origin, true, false);
         }
     }
 
-    WebView mWebView;
+    WebView myWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWebView = (WebView) findViewById(R.id.webView);
 
-        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        mWebView.getSettings().setBuiltInZoomControls(true);
-        mWebView.setWebViewClient(new GeoWebViewClient());
+        myWebView = findViewById(R.id.webView);
 
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.getSettings().setGeolocationEnabled(true);
-        mWebView.setWebChromeClient(new GeoWebChromeClient());
 
-        mWebView.loadUrl("https://7d8ead23.ngrok.io/geoserver/www/map.html");
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)){
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }else{
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.INTERNET)){
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.INTERNET}, 1);
+            }else{
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.INTERNET}, 1);
+            }
+        }
+
+
+
+        // Brower niceties -- pinch / zoom, follow links in place
+        myWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        myWebView.getSettings().setBuiltInZoomControls(true);
+        myWebView.setWebViewClient(new MainActivity.GeoWebViewClient());
+
+        // Below required for geolocation
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setGeolocationEnabled(true);
+        myWebView.setWebChromeClient(new MainActivity.GeoWebChromeClient());
+
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+        myWebView.loadUrl("https://b76a1fcf.ngrok.io/geoserver/www/map.html");
+
+
 
     }
 
-    public void onBackPressed(){
-        if(mWebView.canGoBack()){
-            mWebView.goBack();
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults){
+        switch (requestCode){
+            case 1: {
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                        myWebView.loadUrl("https://b76a1fcf.ngrok.io/geoserver/www/map.html");
+                    }
+                }else{
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
         }
-        else {
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Pop the browser back stack or exit the activity
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
             super.onBackPressed();
         }
     }
